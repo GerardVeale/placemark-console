f9611a7d49908a9b663ae49730f4be17ca387bfd
ddrohan <ddrohan@wit.ie>
2019-02-13 09:37:28 +0000

    MB - Replaced Menu Options with Functions

--- src/main/kotlin/org.wit.placemark.console.main.kt
+++ src/main/kotlin/org.wit.placemark.console.main.kt
@@ -11,9 +11,9 @@ fun main(args: Array<String>) {
     do {
         input = menu()
         when(input) {
-            1 ->  println("You Chose Add Placemark")
-            2 ->  println("You Chose Update Placemark")
-            3 ->  println("You Chose List All Placemarks")
+            1 ->  addPlacemark()
+            2 ->  updatePlacemark()
+            3 ->  listPlacemarks()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
@@ -40,4 +40,16 @@ fun menu() : Int {
     else
         -9
     return option
+}
+
+fun addPlacemark(){
+    println("You Chose Add Placemark")
+}
+
+fun updatePlacemark() {
+    println("You Chose Update Placemark")
+}
+
+fun listPlacemarks() {
+    println("You Chose List All Placemarks")
 }
