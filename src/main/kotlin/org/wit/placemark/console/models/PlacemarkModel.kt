859535205c387644a6cc4700692dba9c00f8e023
ddrohan <ddrohan@wit.ie>
2019-02-21 17:34:02 +0000

    MB - Solution to Lab using Objects and Collection Classes with Add/Update/Search/List Features

---
+++ src/main/kotlin/org/wit/placemark/console/models/PlacemarkModel.kt
@@ -0,0 +1,5 @@
+package org.wit.placemark.console.models
+
+data class PlacemarkModel(var id: Long = 3,
+                          var title: String = "",
+                          var description: String = "")
